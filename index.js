ae2b6a79c85bd701914e91f65e9e431658706b28
c22dev <102235607+c22dev@users.noreply.github.com>
2024-01-08 18:37:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const server = http.createServer((req, res) => {
       res.end('SUCCESS');
     });
   } else {
-    res.end('Geranium Log Server v0.2. Output : Invalid request.');
+    res.end('Geranium Log Server v0.3. Invalid request.');
   }
 });
 
 server.listen(3000, () => {
-  console.log("Geranium Log Server v0.2");
+  console.log("Geranium Log Server v0.3");
   console.log("made by c22dev");
   console.log("");
   console.log("This was made for Geranium. Geranium itself is under GPLv3 license. The license also applies to the log server.");
