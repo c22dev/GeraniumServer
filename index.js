a5597668cc36db8afe77cca71edbe09e02be81d4
c22dev <102235607+c22dev@users.noreply.github.com>
2024-01-08 18:31:08 +0100

    Log instead of store

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
 const http = require('http');
-const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   if (req.method === 'POST') {
@@ -10,22 +8,19 @@ const server = http.createServer((req, res) => {
     });
 
     req.on('end', () => {
-      const randomString = Math.random().toString(36).substring(7);
-      const filename = `data_${randomString}.txt`;
-      fs.writeFile(filename, data, (err) => {
-        if (err) throw err;
-        console.log(`Data saved to ${filename}`);
-      });
-
+      console.log(`${data}`);
       res.end('SUCCESS');
     });
   } else {
     res.end('Geranium Log Server v0.1. Output : Invalid request.');
   }
 });
+
 server.listen(3000, () => {
-  console.log("Geranium Log Server v0.1")
-  console.log("made by c22dev")
-  console.log("")
-  console.log("This was made for Geranium. Geranium itself is under GPLv3 license. The license also apply to the log server.")
+  console.log("Geranium Log Server v0.1");
+  console.log("made by c22dev");
+  console.log("");
+  console.log("This was made for Geranium. Geranium itself is under GPLv3 license. The license also applies to the log server.");
+  console.log("");
+  console.log("");
 });
