d83216f071d4aaf96a0b720409c7abc0094c225f
c22dev <102235607+c22dev@users.noreply.github.com>
2024-09-15 14:06:54 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,38 @@
 from flask import Flask, request
+import os
+import uuid
+import random
+import string
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+log_directory = 'log'
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+def generate_random_string(length=16):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+@app.route('/', methods=['POST'])
 def log_server():
     if request.method == 'POST':
         data = request.get_data(as_text=True)
-        print(data)  # logging could be done another way.
+        
+        uuid_prefix = "UUID "
+        uuid_start = data.find(uuid_prefix) + len(uuid_prefix)
+        uuid_end = data.find("\n", uuid_start)
+        usr_uuid = data[uuid_start:uuid_end] if uuid_start > -1 else 'unknown-uuid'
+
+        random_filename = generate_random_string()
+
+        file_path = os.path.join(log_directory, f"{usr_uuid}_{random_filename}.log")
+
+        with open(file_path, 'w') as log_file:
+            log_file.write(data)
+
         return 'SUCCESS'
     else:
-        return 'Geranium Log Server v0.3. Invalid request.'
+        return 'Invalid request..? If you know what you are doing, contact me at c22dev on Discord.'
 
 if __name__ == '__main__':
     print("Geranium Log Server v0.3")
