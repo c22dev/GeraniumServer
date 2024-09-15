f83b307a4e5a06a06f5dbd07e0b16a56e396b3af
c22dev <102235607+c22dev@users.noreply.github.com>
2024-09-15 14:02:47 +0200

    Update and rename index.js to server.py

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def log_server():
+    if request.method == 'POST':
+        data = request.get_data(as_text=True)
+        print(data)  # Log the POST data
+        return 'SUCCESS'
+    else:
+        return 'Geranium Log Server v0.3. Invalid request.'
+
+if __name__ == '__main__':
+    print("Geranium Log Server v0.3")
+    print("made by c22dev")
+    print("")
+    print("This was made for Geranium. Geranium itself is under GPLv3 license. The license also applies to the log server.")
+    app.run(port=3000)
