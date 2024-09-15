5c6676a26ed5da97cf0d54aa79cac3c0fbba5f60
c22dev <102235607+c22dev@users.noreply.github.com>
2024-09-15 14:10:36 +0200

    Update server.py

--- server.py
+++ server.py
@@ -17,25 +17,27 @@ def generate_random_string(length=16):
 def log_server():
     if request.method == 'POST':
         data = request.get_data(as_text=True)
-        
+        print(data)
+
         uuid_prefix = "UUID "
         uuid_start = data.find(uuid_prefix) + len(uuid_prefix)
         uuid_end = data.find("\n", uuid_start)
-        usr_uuid = data[uuid_start:uuid_end] if uuid_start > -1 else 'unknown-uuid'
+        usr_uuid = data[uuid_start:uuid_end].strip() if uuid_start > -1 else 'unknown-uuid'
 
         random_filename = generate_random_string()
 
-        file_path = os.path.join(log_directory, f"{usr_uuid}_{random_filename}.log")
+        file_name = f"{usr_uuid}_{random_filename}.log"
+        file_path = os.path.join(log_directory, file_name)
 
         with open(file_path, 'w') as log_file:
-            log_file.write(data)
+            log_file.write(data.replace('\\n', '\n'))
 
         return 'SUCCESS'
     else:
-        return 'Invalid request..? If you know what you are doing, contact me at c22dev on Discord.'
+        return 'Invalid request..? If you know what you are doing contact me at c22dev on Discord.'
 
 if __name__ == '__main__':
-    print("Geranium Log Server v0.3")
+    print("Geranium Log Server v0.4")
     print("made by c22dev")
     print("")
     print("This was made for Geranium. Geranium itself is under GPLv3 license. The license also applies to the log server.")
